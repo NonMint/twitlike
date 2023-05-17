4f8c4bb0d70e189799a714723b6e29896e6a65ef
Ian Phillipchuk <nonmint@gmail.com>
2023-05-17 11:10:11 -0600

    Pushing again (Ryan don't read these messages)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,16 @@
 import Image from 'next/image'
 import './TwitterFeed.css';
-
-const Tweet = ({ username, content, date }: any) => {
-  return (
-    <div className="bg-white rounded-lg shadow p-4 mb-4">
-      <div className="flex items-center mb-2">
-        <img
-          className="w-12 h-12 rounded-full mr-4"
-          src="https://placekitten.com/50/50"
-          alt="User Avatar"
-        />
-        <div>
-          <span className="font-bold text-black">{username}</span>
-          <span className="text-black text-sm ml-1">{date}</span>
-        </div>
-      </div>
-      <p className="text-lg text-black">{content}</p>
-    </div>
-  );
-};
+import Tweet from './tweet';
+import TwitterSidebar from './TwitterSidebar';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="twitter-feed">
-        <div className="col-span-1 bg-gray-200 p-4">
-          <h2>Column 1</h2>
-          {/* Add tweets or content for column 1 */}
+        <div className="col-span-1 p-4">
+          <TwitterSidebar />
         </div>
-        <div className="col-span-1 bg-gray-200 p-4">
+        <div className="col-span-1 p-4">
           <h2>Column 2</h2>
           <Tweet
             username="JohnDoe"
@@ -41,7 +23,7 @@ export default function Home() {
             date="May 16, 2023"
           />
         </div>
-        <div className="col-span-1 bg-gray-200 p-4">
+        <div className="col-span-1 p-4">
           <h2>Column 3</h2>
           {/* Add tweets or content for column 3 */}
         </div>
